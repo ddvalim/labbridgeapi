8001dc78716eda7668bcccb74dbc83bb02320d5b
Diovana <diovanavalimm@gmail.com>
2020-09-14 00:05:58 -0300

    first commit

---
+++ src/main/java/com/diovana/labbridgeapi/LabbridgeapiApplication.java
@@ -0,0 +1,13 @@
+package com.diovana.labbridgeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabbridgeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabbridgeapiApplication.class, args);
+	}
+
+}
